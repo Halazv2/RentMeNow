1c24b7ee99b265a92ed8c8f53555f4ebfb0fece9
Halazv2 <halaziani2003@gmail.com>
2021-12-31 22:26:09 +0100

    aalmost done

--- breif3/js.js
+++ breif3/js.js
@@ -2,7 +2,36 @@
 // function myFunction() {
 //     alert("input");
 //   }
-document.querySelector("#send").addEventListener("click", function() {
-document.querySelectorAll("input").forEach(input => alert(`${input.name}: ${input.value}`));
-});
+// document.querySelector("#send").addEventListener("click", function() {
+// document.querySelectorAll("input").forEach(input => alert(`${input.name}: ${input.value}`));
+// });
     
+
+
+    //gallerie
+
+    const filterItems = document.querySelector(".items");
+    const filterImg = document.querySelectorAll(".imagee");
+
+    window.onload=()=>{ 
+        filterItems.onclick =(selectedItem)=>{
+            if(selectedItem.target.classList.contains("item")){
+                // console.log("true");
+                filterItems.querySelector(".active").classList.remove("active");
+                selectedItem.target.classList.add("active");
+                let filterName =selectedItem.target.getAttribute("data-name");
+                // console.log(filterName);
+                filterImg.forEach((imagee)=>{
+                    let filterImages = imagee.getAttribute("data-name"); 
+                    // console.log(filterImages);
+                    if((filterImages==filterName) || filterName =="all"){
+                        imagee.classList.add("show");
+                        imagee.classList.remove("hide");
+                    }else{
+                        imagee.classList.add("hide");
+                        imagee.classList.remove("show");
+                    }
+                });
+            }
+        }
+    }
