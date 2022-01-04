e46bb325203faf8c89ebb662ee003741f4673225
Halazv2 <halaziani2003@gmail.com>
2022-01-04 16:59:41 +0100

    done

--- breif3/js.js
+++ breif3/js.js
@@ -4,34 +4,34 @@
 //     .forEach((input) => alert(`${input.name}: ${input.value}`));
 // });
 
-// document.querySelector(".form").addEventListener("submit", function modal() {
-//   var name = document.querySelector("#fname").value;
-//   var lname = document.querySelector("#lname").value;
-//   var email = document.querySelector("#email").value;
-//   var phone = document.querySelector("#phone").value;
-//   var message = document.querySelector("#message").value;
-
-//   alert("Name : " + name+ "\nFirst: "+lname+ "\nmail : " + email + "\nPhone Number : " + phone+"\nMessage: "+message);
-// });
-
 document.querySelector(".form").addEventListener("submit", function modal() {
   var name = document.querySelector("#fname").value;
   var lname = document.querySelector("#lname").value;
   var email = document.querySelector("#email").value;
   var phone = document.querySelector("#phone").value;
   var message = document.querySelector("#message").value;
-  Swal.fire(
-    "Success!",
-    "Name : " +
-      name +
-      "\nFirst: " +
-      lname +
-      "\nmail : " +
-      email +
-      "\nPhone Number : " +
-      phone +
-      "\nMessage: " +
-      message,
-    "success"
-  );
+
+  alert("Name : " + name+ "\nFirst: "+lname+ "\nmail : " + email + "\nPhone Number : " + phone+"\nMessage: "+message);
 });
+
+// document.querySelector(".form").addEventListener("submit", function modal() {
+//   var name = document.querySelector("#fname").value;
+//   var lname = document.querySelector("#lname").value;
+//   var email = document.querySelector("#email").value;
+//   var phone = document.querySelector("#phone").value;
+//   var message = document.querySelector("#message").value;
+//   Swal.fire(
+//     "Success!",
+//     "Name : " +
+//       name +
+//       "\nFirst: " +
+//       lname +
+//       "\nmail : " +
+//       email +
+//       "\nPhone Number : " +
+//       phone +
+//       "\nMessage: " +
+//       message,
+//     "success"
+//   );
+// });
